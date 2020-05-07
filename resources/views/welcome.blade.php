ec4db37fc13fedfa58edcb9935d801e25aa589c0
TanoueDaisuke <tano.dai.0711@icloud.com>
2020-05-07 18:36:27 +0900

    headerのhtml, scss実装とコンパイルによるjsファイルの出力(webpack)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,18 +4,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
     <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
     <title>Lara Stagram</title>
 </head>
 <body>
     <header>
-        <h1>Lara Stagram</h1>
-        <nav>
-            <ul>
-                <li>投稿</li>
-                <li>(顔アイコン)</li>
-            </ul>
-        </nav>
+        <div class="header_inner">
+            <h1>
+                <a href="/"><img src="https://fontmeme.com/permalink/200507/5ef500071d1e8087e12a53f96a5b3fd8.png" alt="lara stagram" border="0"></a>
+            </h1>
+            <nav>
+                <ul>
+                    <li class="post_btn">
+                        <a href="#">投稿</a>
+                    </li>
+                    <li class="profile_btn">
+                        <a href="#"><i class="fas fa-user-alt"></i></a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
     </header>
 </body>
 </html>
