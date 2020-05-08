6f36d9a8a0cf72d98e71338eaee2de546dd4f63a
TanoueDaisuke <tano.dai.0711@icloud.com>
2020-05-08 11:14:44 +0900

    #14 usersのダミー情報をseederで追加

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'test',
+            'email' => 'test@test.com',
+            'password' => bcrypt('testtest'), // bcrypt関数で暗号化して保存する
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+    }
+}
