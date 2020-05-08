5529fbbe17b5e7e0a3f0f7abfbbe8c0c0132ed22
TanoueDaisuke <tano.dai.0711@icloud.com>
2020-05-08 15:26:14 +0900

    #14 フォルダ階層変更

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,73 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+    <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+    <title>Lara Stagram</title>
+</head>
+<body>
+<header>
+    <div class="header_inner">
+        <h1>
+            <a href="/"><img src="https://fontmeme.com/permalink/200507/5ef500071d1e8087e12a53f96a5b3fd8.png" alt="lara stagram" border="0"></a>
+        </h1>
+        <nav>
+            <ul>
+                <li class="post_btn">
+                    <a href="#">投稿</a>
+                </li>
+                <li class="profile_btn">
+                    <a href="#"><i class="fas fa-user-alt"></i></a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</header>
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+<main>
+    <div class="post_wrapper">
+        <div class="post_top">   
+            <div class="left">
+                <a href="#"><img src="{{ asset('/img/nadeshiko.png') }}" alt=""> {{-- 投稿ユーザーのアイコン --}}</a>
+                <a href="#" class="hover"><p>投稿ユーザー</p></a> {{-- 投稿ユーザー名 --}}                
+            </div>                 
+            <a href="#"><i class="fas fa-trash-alt"></i></a> {{-- 投稿削除アイコン --}}
+        </div>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+        <img src="{{ asset('/img/rainbow_bridge.jpg') }}" alt=""> {{-- 投稿写真 --}}
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+        <div class="post_bottom_wrapper">
+            <div class="inner">
+                <div class="icons">                                
+                    <a href="#"><i class="far fa-heart"></i></a> {{-- いいねアイコン(ハート) --}}
+                    <a href="#" class="comment"><i class="far fa-comment"></i></a> {{-- コメントアイコン --}}
+                </div>
+        
+                <div class="post_bottom">
+                    <p><span>〇〇</span> が「いいね！」しました</p>
+                    <div class="comments">
+                        {{-- ここに複数のコメント表示 --}}
+                        <p><span>〇〇</span> 写真撮った！(例)</p>
+                        <p><span>△△</span> きれい！(例)</p>
+                    </div>    
+                    <p class="date">2020-05-07 13:12:36</p>            
                 </div>
             </div>
         </div>
+    
+        {{-- コメント作成 --}}
+        <form method="GET" action="#">                
+            @csrf {{-- Cross-Site Request Forgeriesの対策 --}}
+            <input placeholder="コメント ..." type="text" name="message">
+        </form>
     </div>
-</div>
-@endsection
+</main>
+
+<footer>
+    <p>Copyright &copy; HaseTano </p>
+</footer>
+</body>
+</html>
