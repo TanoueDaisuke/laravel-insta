884151dec85d55893055b34efb1bf13ca8f2753d
TanoueDaisuke <tano.dai.0711@icloud.com>
2020-05-09 18:22:45 +0900

    #18 モデルファイルのリレーションリファクタリング

--- app/Post.php
+++ app/Post.php
@@ -6,7 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
-    protected $table = 'posts';
-    
+    // リレーション定義
+    public function likes() {
+        return $this->hasMany('App\Like');
+    }
+
+    public function comments() {
+        return $this->hasMany('App\Comment');
+    }
+
+    public function user() {
+        return $this->belongsTo('App\User');
+    }
 }
