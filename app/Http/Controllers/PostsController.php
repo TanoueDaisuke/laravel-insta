515c0753f61474cca6b7dade249bd9fb40ef5ba5
TanoueDaisuke <tano.dai.0711@icloud.com>
2020-05-09 18:43:30 +0900

    #18 postのcontentのみのcrudのc機能実装

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class PostsController extends Controller
 {
-    //
+    public function create()
+    {
+        return view('posts/create');
+    }
+
+    public function store(Request $request) {
+        $post = new Post();
+        $post->content = $request->content;
+
+        Auth::user()->posts()->save($post);
+
+        return redirect()->route('top');
+    }
 }
