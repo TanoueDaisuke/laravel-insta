5501f067d6d41a88e0f600cb147c14b2532cd9e4
TanoueDaisuke <tano.dai.0711@icloud.com>
2020-05-17 09:57:50 +0900

    #30 コメントの投稿ができるようにする

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Comment;
+use App\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CommentsController extends Controller
 {
-    //
+    public function store(Request $request, Post $post)
+    {
+        $comment = new Comment();
+        $comment->message = $request->message;
+        $comment->post_id = $post->id;
+
+        Auth::user()->comments()->save($comment);
+
+        return redirect()->route('top');
+    }
 }
